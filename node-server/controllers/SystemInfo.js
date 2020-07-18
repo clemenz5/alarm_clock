abafdc637888c3316718f7a8ddd77ab247252406
Lorenz Keefer <clemenzkafer@gmail.com>
2020-07-18 09:34:27 +0200

    minifix

--- node-server/controllers/SystemInfo.js
+++ node-server/controllers/SystemInfo.js
@@ -1,24 +1,23 @@
-'use strict';
+"use strict";
 
-var utils = require('../utils/writer.js');
-var SystemInfo = require('../service/SystemInfoService');
+var utils = require("../utils/writer.js");
+var SystemInfo = require("../service/SystemInfoService");
 
-module.exports.checkTemp = function checkTemp (req, res, next) {
+module.exports.checkTemp = function checkTemp(req, res, next) {
   SystemInfo.checkTemp()
     .then(function (response) {
+      const { exec } = require("child_process");
+      exec("vcgencmd measure_temp", (err, stdout, stderr) => {
+        if (err) {
+          // node couldn't execute the command
+          return;
+        }
 
-      const { exec } = require('child_process');
-      exec('vcgencmd measure_temp', (err, stdout, stderr) => {
-      if (err) {
-      // node couldn't execute the command
-      return;
-    }
-
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    response = '${stdout}'
-    console.log(`stderr: ${stderr}`);
-    });
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        response = `${stdout}`;
+        console.log(`stderr: ${stderr}`);
+      });
       utils.writeJson(res, response);
     })
     .catch(function (response) {
