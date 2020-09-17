550cb89abd2f868ec718d04286a2fa72fdb5771f
Lorenz Keefer <clemenzkafer@gmail.com>
2020-09-17 09:00:47 +0200

    added a few styles

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PageListComponent } from './page-list/page-list.component';
 import { TemplateAlarmClockComponent } from './_template/template-alarm-clock/template-alarm-clock.component';
+import { TemplateAlarmClockFormComponent } from './_template/template-alarm-clock-form/template-alarm-clock-form.component';
+import { TemplateHeaderComponent } from './_template/template-header/template-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageListComponent,
-    TemplateAlarmClockComponent
+    TemplateAlarmClockComponent,
+    TemplateAlarmClockFormComponent,
+    TemplateHeaderComponent
   ],
   imports: [
     BrowserModule
